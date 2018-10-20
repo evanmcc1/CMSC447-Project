e7d73bf67c5e6a84a50a4684db1cfb8a3dea2245
KevinRapa <kevjrapa@gmail.com>
2018-10-20 10:56:27 -0400

    Added filter by radius method

--- src/js/map.js
+++ src/js/map.js
@@ -1,28 +1,28 @@
 var __testEvents = [
-	{
-		"lat": 		39.123,
-		"long":     -76.824,
-		"severity": 5,
-		"class":    "fire"
-	},
-	{
-		"lat": 		39.125,
-		"long":     -76.822,
-		"severity": 3,
-		"class":    "flood"
-	},
-	{
-		"lat": 		39.122,
-		"long":     -76.821,
-		"severity": 2,
-		"class":    "animal"
-	},
-	{
-		"lat": 		39.132,
-		"long":     -76.811,
-		"severity": 3,
-		"class":    "animal"
-	}
+    {
+        "lat":         39.123,
+        "long":     -76.824,
+        "severity": 5,
+        "class":    "fire"
+    },
+    {
+        "lat":         39.125,
+        "long":     -76.822,
+        "severity": 3,
+        "class":    "flood"
+    },
+    {
+        "lat":         39.122,
+        "long":     -76.821,
+        "severity": 2,
+        "class":    "animal"
+    },
+    {
+        "lat":         39.132,
+        "long":     -76.811,
+        "severity": 3,
+        "class":    "animal"
+    }
 ];
 
 // Namespace
@@ -44,6 +44,8 @@ var NS = {
         offset: L.point(10,10),
         className: 'tooltip'
     }
+    LAT_MAGIC: 68.69,  // Magic constant for turning degree distance to miles.
+    LNG_MAGIC: 69.17   // Magic constant for turning degree distance to miles.
 };
 
 
@@ -55,17 +57,12 @@ var LF_MAP = (function() {
     map.allEvents = [];  // All events
 
     map.shapes = {
-    	points: [],   // The current points on the map.
-    	lines: [],    // The current lines on the map.
-    	polygon: null // The current polygon on the map.
+        points: [],   // The current points on the map.
+        lines: [],    // The current lines on the map.
+        polygon: null // The current polygon on the map.
     };
     
-    /**
-     * Switches up all markers on the map based on filter parameters.
-     * @param conditions {object} Mapping of marker field names to filter parameters.
-     * Currently handles only format (this AND this AND this AND ...)
-     */
-    map.filter = function(conditions) {
+    map.display = function(events) {
         var bounds = []; // Used to fit map to markers.
 
         // Reset map
@@ -75,21 +72,19 @@ var LF_MAP = (function() {
         this.displayed = [];
 
         // Add the new markers
-        for (var marker of this.allEvents) {
-        	var matches = true;
-
-            for (var key of Object.keys(conditions)) {
-                if (marker[key] != conditions[key]) {
-                	matches = false;
-                    break; // Marker doesn't meet conditions.
+        for (var event of events) {
+            var marker = L.marker([event['lat'], event['long']]);
+            
+            for (var field of Object.keys(event)) {
+                if (field != "lat" && field != "long") {
+                    // Puts ALL fields in marker. Maybe not necessary to do all?
+                    marker[field] = event[field];
                 }
             }
 
-            if (matches) {
-	            this.displayed.push(marker);
-	            marker.addTo(this);
-	            bounds.push(marker.getLatLng());
-        	}
+            this.displayed.push(marker);
+            bounds.push(marker.getLatLng())
+            marker.addTo(this);
         }
 
         // Fit the view to the scope of the markers
@@ -98,25 +93,22 @@ var LF_MAP = (function() {
         }
     };
 
+
     /**
-     * Adds all the events from json data to the map.
-     * @param events {array} array of event objects.
+     * Displays only markers withing r degrees of lat and lng.
+     * @param lat {number} Latitude.
+     * @param lng {number} Longitude.
+     * @param mi {number} Radius in miles.
      */
-    map.populate = function(events) {
-        for (var event of events) {
-            var marker = L.marker([event['lat'], event['long']]);
-            
-            for (var field of Object.keys(event)) {
-            	if (field != "lat" && field != "long") {
-            		// Puts ALL fields in marker. Maybe not necessary to do all?
-            		marker[field] = event[field];
-            	}
-            }
-
-            this.allEvents.push(marker);
-        }
-
-        this.filter({}); // Add all markers to map.
+    map.filterByRadius = function(lat, lng, mi) {
+    	for (var marker of this.displayed) {
+    		var coords = marker.getLatLng();
+    		var a = Math.abs(lat - coords.lat) * NS.LAT_MAGIC;
+    		var b = Math.abs(lng - coords.lng) * NS.LNG_MAGIC * Math.cos(lat);
+    		var isIn = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2)) <= mi
+    			
+    		isIn ? marker.addTo(this) : marker.remove();
+    	}
     }
 
 
@@ -124,54 +116,54 @@ var LF_MAP = (function() {
      * Adds a point to the map for drawing lines.
      */
     map.on('click', function(e) {
-    	var pts = this.shapes.points;
+        var pts = this.shapes.points;
 
-    	if (this.shapes.points.length) {	
-    		var line = L.polyline([pts[pts.length - 1], e.latlng], 
-    			{color: "red"});
-    		line.addTo(this);
-    		this.shapes.lines.push(line);
-    	}
+        if (this.shapes.points.length) {    
+            var line = L.polyline([pts[pts.length - 1], e.latlng], 
+                {color: "red"});
+            line.addTo(this);
+            this.shapes.lines.push(line);
+        }
 
-    	pts.push(e.latlng);
+        pts.push(e.latlng);
     });
 
 
-    var insidePolygon = function(coords, x, y) {
-    	return true;
-	}
+    var insidePolygon = function(coords, lat, lng) {
 
-	/*
-	 * Completes a polygon.
-	 */
+    }
+
+    /*
+     * Completes a polygon.
+     */
     map.on('dblclick', function(e) {
-    	if (this.shapes.polygon) {
-    		this.shapes.polygon.remove();
-    		this.shapes.polygon = null;
-    	}
+        if (this.shapes.polygon) {
+            this.shapes.polygon.remove();
+            this.shapes.polygon = null;
+        }
 
-    	if (this.shapes.points.length > 2) {
-    		var poly = L.polygon(this.shapes.points, {color: "red"}); 
-			this.shapes.polygon = poly;
-			poly.addTo(this);
+        if (this.shapes.points.length > 2) {
+            var poly = L.polygon(this.shapes.points, {color: "red"}); 
+            this.shapes.polygon = poly;
+            poly.addTo(this);
 
-			for (var marker of this.displayed) {
-	    		var coords = marker.getLatLng();
+            for (var marker of this.displayed) {
+                var coords = marker.getLatLng();
 
-	    		if (! insidePolygon(this.shapes.points, coords.lng, coords.lat)) {
-	    			// remove marker
-	    		}
-	    	}
-    	}
-    	
-    	this.shapes.points = [];
-    	for (var line of this.shapes.lines) {
-    		line.remove();
-    	}
-    	this.shapes.lines = [];
+                if (! insidePolygon(this.shapes.points, coords.lat, coords.lng)) {
+                    // remove marker
+                }
+            }
+        }
+        
+        this.shapes.points = [];
+        for (var line of this.shapes.lines) {
+            line.remove();
+        }
+        this.shapes.lines = [];
     });
 
     return map;
 })();
 
-LF_MAP.populate(__testEvents);
+LF_MAP.display(__testEvents);
