341e3056190cf67e3dc4e6047d83d225b7f22d32
Adam Huganir <huganir1@umbc.edu>
2018-10-22 11:29:09 -0400

    removed some old files, started php request scripts

--- html/php/sample_query.php
+++ html/php/sample_query.php
@@ -7,9 +7,25 @@
 
 $sample = array(
             array(
-                'id' => 100,
-                'title' => 'howdy'
+                'event_id' => 100,
+                'title' => 'This is a ticket title',
+                'comment' => 'This is the original comment',
+                'category' => 'This is the category name',
+                'keywords' => array('keyword 0 title','keyword 3','keyword 7','keyword 2',),
+                'keyword_ids' => array(0,3,7,2),
+                'lat' => 39.253428,
+                'long' => -76.704070
+            ),
+            array(
+                'event_id' => 101,
+                'title' => 'This is a ticket title again',
+                'comment' => 'This is the original comment again',
+                'category' => 'This is the category name again',
+                'keywords' => array('keyword 1 title','keyword 2','keyword 4','keyword 5',),
+                'keyword_ids' => array(1,2,4,5),
+                'lat' => 39.255252,
+                'long' =>  -76.717229,
             )
 );
 header('Content-Type: application/json');
-echo json_encode($data);
+echo json_encode($sample);
---
+++ html/php/search.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Adam Huganir
+ * Date: 10/22/2018
+ * Time: 11:28 AM
+ */
+
+$servername = "localhost";
+$username = "web";
+$password = "platypus";
+$dbname = "rrdb";
