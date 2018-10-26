8b6a56cb38a4d8083049f001e688a1ad86d80449
KevinRapa <kevjrapa@gmail.com>
2018-10-25 21:57:19 -0400

    cleaned up code, abstracted some stuff away

--- html/js/mapInterface.js
+++ html/js/mapInterface.js
@@ -1,6 +1,12 @@
-// Namespace
-var NS = {
-    OM_TABULAR_OPTIONS: {
+/**
+ * The DATA_HANDLER is responsible for keeping the map and tables 
+ * synchronized. Each handler on the page which affets events should invoke a
+ * method of EVENT_HANDLER. The event handler will deal with the map and tables.
+ * The DATA_HANDLER is the middleman between the user and the map/table.
+ */
+var OM_DATA_HANDLER = (function() {
+
+	var OM_TABULAR_OPTIONS = {
 		layout:"fitColumns",
 		columns:[
 			{title:"Id", field:"id"},
@@ -9,67 +15,65 @@ var NS = {
 		 	{title:"Recieved", field:"recieved"}
 		],
 		rowClick:function(e, row){ //trigger an alert message when the row is clicked
-	 		OM_TABLE_MAP_SYNCHRONIZER.migrate(row);
-	 	},
-	},
-	OC_TABULAR_OPTIONS: {
+	 		DATA_HANDLER.migrate(row);
+	 	}
+	};
 
+	// Leaflet map, event feed, and grouped events feed.
+	var LF_MAP = rrLeafletMap("leaflet_map");
+	var EVENT_FEED = new Tabulator("#filtered_event_feed", OM_TABULAR_OPTIONS);
+	var GROUPED_EVENTS = new Tabulator("#grouped_events_table", OM_TABULAR_OPTIONS);
+
+	var	DATA_HANDLER = {
+	    // Queries data from the DB and then displays them on the page. 
+	    query: function(parameters) {
+	        // Query json object from DB
+	        LF_MAP.addAllEvents(parameters);
+	        EVENT_FEED.setData(parameters);
+	    },
+
+	    // Marks event as selected, opens tooltip, highlights row.
+	    select: function(row) {
+	        LF_MAP.describeMarker(row.firstChild.textContent);
+	    },
+
+	    // Moves the event from its event table to the other 
+	    migrate: function(row) {
+	        if (EVENT_FEED.getRowPosition(row) > -1) {
+	        	row.delete();
+	            GROUPED_EVENTS.addRow(row.getData());
+	            LF_MAP.markSelected(row._row.data.id, true); // Shouldnt be accessing this??
+	        }
+	        else if (GROUPED_EVENTS.getRowPosition(row) > -1) {
+	        	row.delete();
+	        	EVENT_FEED.addRow(row.getData());
+	            LF_MAP.markSelected(row._row.data.id, false);
+	        }
+	    }
+	};
+
+	for (var table of document.getElementsByClassName("tabulator-table")) {
+		table.addEventListener('mouseover', function(e) {
+			DATA_HANDLER.select(e.target.parentNode);
+		});
 	}
-};
+
+	return DATA_HANDLER;
+})();
 
 
-// LEAFLET MAP OBJECT
-var LF_MAP = rrLeafletMap("leaflet_map");
 
-// OM table objects
-var EVENT_FEED = new Tabulator("#filtered_event_feed", NS.OM_TABULAR_OPTIONS);
-var GROUPED_EVENTS = new Tabulator("#grouped_events_table", NS.OM_TABULAR_OPTIONS);
+var OC_DATA_HANDLER = (function() {
+	var OC_TABULAR_OPTIONS = {
 
-/**
- * Since the map and table both hold events, but each cares only about certain
- * kinds of event information, the OM_TABLE_MAP_SYNCHRONIZER is responsible for keeping them 
- * synchronized. Each handler on the page which affets events should invoke a
- * method of EVENT_HANDLER. The event handler will deal with the map and tables.
- * The OM_TABLE_MAP_SYNCHRONIZER is the middleman between the user and the map/table.
- */
-var OM_TABLE_MAP_SYNCHRONIZER = {
-    selected: null,
+	};
 
-    // Queries data from the DB and then displays them on the page. 
-    query: function(parameters) {
-        // Query json object from DB
-        LF_MAP.addAllEvents(parameters);
-        EVENT_FEED.setData(parameters);
-    },
+	return null;
+})();
 
-    // Marks event as selected, opens tooltip.
-    select: function(row) {
-        LF_MAP.describeMarker(row.firstChild.textContent);
-    },
-
-    // Moves the event from its event table to the other 
-    migrate: function(row) {
-        if (EVENT_FEED.getRowPosition(row) > -1) {
-        	row.delete();
-            GROUPED_EVENTS.addRow(row.getData());
-            LF_MAP.markSelected(row._row.data.id, true); // Shouldnt be accessing this??
-        }
-        else if (GROUPED_EVENTS.getRowPosition(row) > -1) {
-        	row.delete();
-        	EVENT_FEED.addRow(row.getData());
-            LF_MAP.markSelected(row._row.data.id, false);
-        }
-    }
-};
-
-for (var table of document.getElementsByClassName("tabulator-table")) {
-	table.addEventListener('mouseover', function(e) {
-		OM_TABLE_MAP_SYNCHRONIZER.select(e.target.parentNode);
-	})
-}
 
 //// TESTING ///////////////////////
-OM_TABLE_MAP_SYNCHRONIZER.query([
+OM_DATA_HANDLER.query([
 {
     "id":123, 
     "name":123, 
