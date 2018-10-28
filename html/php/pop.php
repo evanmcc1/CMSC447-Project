67ac086af56d3c7c4b61ccaee731d74d9ea546bd
Adam Huganir <huganir1@umbc.edu>
2018-10-28 19:57:58 -0400

    adding degree/mile conversion

--- html/php/pop.php
+++ html/php/pop.php
@@ -20,19 +20,24 @@ $username = "web";
 $password = "platypus";
 $dbname = "rrdb";
 
-echo "hi, thank you for " . key($_POST);
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    echo "Error: Failed to make a MySQL connection, here is why: \n";
+    echo "Errno: " . $conn->connect_errno . "\n";
+    echo "Error: " . $conn->connect_error . "\n";
+    die("Connection failed: " . $conn->connect_error);
+}
+
+function sql_safe(&$item, $key){
+    global $conn;
+    $item = $conn->real_escape_string($item);
+}
 
 if (key($_POST) == "add_caller") {
     echo "\n:: running " . key($_POST);
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        echo "Error: Failed to make a MySQL connection, here is why: \n";
-        echo "Errno: " . $conn->connect_errno . "\n";
-        echo "Error: " . $conn->connect_error . "\n";
-        die("Connection failed: " . $conn->connect_error);
-    }
+
     $q = <<<EOT
     INSERT INTO callers(fname, lname, phone) values('{$_POST["fname"]}','{$_POST["lname"]}',{$_POST["phone"]});
 EOT;
@@ -50,15 +55,77 @@ EOT;
     $conn->close();
 }
 
-
-
 if (key($_POST) == "add_event") {
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+//    echo ":: running " . key($_POST);
+//    echo "<br>";
+//    print_r($_POST);
+//    echo "<br>";
+    array_walk($_POST, sql_safe);
+    $q = <<<EOT
+    INSERT INTO callers(fname, lname, phone) values(
+    '{$_POST["callers_fname"]}',
+    '{$_POST["callers_lname"]}',
+    {$_POST["callers_phone"]})
+EOT;
+    echo "\n:: SQL = " . $q;
+    echo "<br>";
+    if (!$conn->query($q)) {
+        echo "\n:: Insert failed: (" . $conn->errno . ") " . $conn->error;
+        echo "<br>";
     }
+    $caller_id = $conn->insert_id;
+
+    $q = <<<EOT
+    INSERT INTO event_tickets(title_old) values('{$_POST["event_info_title"]}')
+EOT;
+//    echo "\n:: SQL = " . $q;
+//    echo "<br>";
+    if (!$conn->query($q)) {
+        echo "\n:: Insert failed: (" . $conn->errno . ") " . $conn->error;
+        echo "<br>";
+    }
+    $event_id = $conn->insert_id;
+
+    $q = <<<EOT
+    INSERT INTO caller_events(caller_events.event_id, caller_events.caller_id) values(
+    {$event_id}, {$caller_id})
+EOT;
+//    echo "\n:: SQL = " . $q;
+//    echo "<br>";
+    if (!$conn->query($q)) {
+        echo "\n:: Insert failed: (" . $conn->errno . ") " . $conn->error;
+        echo "<br>";
+    }
+
+    $q = <<<EOT
+    INSERT INTO event_info(event_info.event_id, event_info.title, 
+                           event_info.priority, event_info.body,
+                           event_info.category_id, event_info.lat, 
+                           event_info.long, event_info.address,
+                           event_info.city, event_info.state,
+                           event_info.zip, event_info.user_id, event_info.rad) values(
+    {$event_id}, '{$_POST["event_info_title"]}', {$_POST["event_info_priority"]},
+    '{$_POST["event_info_body"]}', {$_POST["event_info_category_id"]}, 
+    {$_POST["event_info_lat"]}, {$_POST["event_info_long"]}, '{$_POST["event_info_address"]}',
+    '{$_POST["event_info_city"]}', '{$_POST["event_info_state"]}', {$_POST["event_info_zip"]}, 
+    {$_POST["event_info_user_id"]}, {$_POST["event_info_rad"]})
+EOT;
+//    echo "\n:: SQL = " . $q;
+//    echo "<br>";
+    if (!$conn->query($q)) {
+        echo "\n:: Insert failed: (" . $conn->errno . ") " . $conn->error;
+        echo "<br>";
+    }
+
+
+    $conn->commit();
+    $conn->close();
+}
+
+
+if (key($_POST) == "other") {
+    // Create connection
+
     $q = <<<EOT
     SELECT * from event_tickets WHERE title_old LIKE '%{$_POST["title"]}%'
 EOT;
